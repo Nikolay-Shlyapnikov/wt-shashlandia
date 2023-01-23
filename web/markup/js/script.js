f8c1890b51906fefb7f916efb277a463ebbe9ec4
Anatolev Maksim <anatolev.web@icloud.com>
2023-01-24 01:10:30 +0300

    Delete web/markup directory

--- web/markup/js/script.js
+++
@@ -1,9 +0,0 @@
-const headerBurgerButton = document.querySelector('.page-header__burger');
-const headerNav = document.querySelector('.page-header__nav');
-const headerLogo = document.querySelector('.page-header__logo');
-
-headerBurgerButton.addEventListener('click', () => {
-    headerBurgerButton.classList.toggle('active');
-    headerNav.classList.toggle('active');
-    headerLogo.classList.toggle('active');
-});
